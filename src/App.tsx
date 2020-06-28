8cddada5ac6e46ce1355bd85373b77b7b513b26c
Kerminate <kepeilin@outlook.com>
2020-06-28 10:17:14 +0800

    feat: 删除无用代码，添加基础样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>123</h1>
+        <h2>123</h2>
+        <h3>123</h3>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
